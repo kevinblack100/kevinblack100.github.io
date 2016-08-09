83c1d7bbb270ad631aa6e4d7471bb5c6983762bb
Kevin <kevinblack100@gmail.com>
2016-08-09 16:36:33 +0000

    cracker: draw grid with guesses

--- cracker/cracker.js
+++ cracker/cracker.js
@@ -1,3 +1,6 @@
+// ========================================================
+// Data
+
 const colors = [
     'blue',
     'red',
@@ -8,6 +11,18 @@ const colors = [
 ];
 
 var currentColor = colors[0];
+
+
+const codeLength = 4;
+// indices into the colors array
+const guesses = [
+    [ 0, 1, 2, 3 ] // dummy data for testing
+    ];
+const maxNumGuesses = 10;
+
+
+// ========================================================
+// Buttons
 
 function setColor(colorIndex) {
     if (colorIndex > 0 && colorIndex < colors.length)
@@ -32,6 +47,46 @@ function setupButtons() {
     }
 }
 
+// ========================================================
+// Grid
+
+function drawGuessGrid() {
+    const gridContainer = document.getElementById('guessGrid');
+    const N = guesses.length;
+    for (var r = 0, M = maxNumGuesses; r < M; ++r) {
+        const row = document.createElement('tr');
+        const cells = [];
+        for (var c = 0, C = codeLength; c < C; ++c) {
+            const cell = document.createElement('td');
+            // TODO: take out debug string
+            cell.innerHTML = 'r: ' + r + ' c: ' + c;
+            row.appendChild(cell);
+            
+            cells.push(cell);
+        }
+        const guessIndex = (M - r - 1);
+        if (guessIndex < N) {
+            const guess = guesses[guessIndex];
+            for (var c = 0, C = guess.length; c < C; ++c) {
+                const cell = cells[c];
+                const colorIndex = guess[c];
+                const color = colors[colorIndex];
+                cell.className = color;
+                cell.innerHTML = color;
+            }
+        }
+        else {
+            // leave the row blank
+            // TODO set up onclick handlers
+        }
+        gridContainer.appendChild(row);
+    }
+}
+
+// ========================================================
+// General Setup
+
 window.addEventListener('load', function() {
     setupButtons();
+    drawGuessGrid();
 }, false);
